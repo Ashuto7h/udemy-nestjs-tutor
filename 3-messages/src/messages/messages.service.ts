9139d566c09d08e222616328d26b4474cdbbfd74
Ashuto7h <ashusahu198@gmail.com>
2023-01-01 11:53:08 +0530

    added dependency-injection

---
+++ 3-messages/src/messages/messages.service.ts
@@ -0,0 +1,16 @@
+import { MessagesRepository } from './messages.repository';
+import {Injectable} from '@nesjts/common'
+@Injectable()
+export class MessagesService {
+  constructor(public messagesRepo: MessagesRepository) {
+  }
+  async findOne(id: string) {
+    return this.messagesRepo.findOne(id);
+  }
+  async findAll() {
+    return this.messagesRepo.findAll();
+  }
+  async create(content: string) {
+    return this.messagesRepo.create(content);
+  }
+}
